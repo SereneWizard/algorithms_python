43db3fc9a106a3cd1f07402ff9f010c241e7218f
Vaskar Dahal <dahalvaskar@gmail.com>
2020-01-07 16:50:20 -0800

    First commit

---
+++ mergeII.py
@@ -0,0 +1,20 @@
+
+def merge(A, l, m, r):
+    nl = m-l+1
+    n2 = r-m
+
+    L = [0] * n1
+    R = [0] * n2
+
+   i, j = 0, 0
+    while i<=n1 and j<=n2:
+        if A[i] > A[n2+j]
+        
+
+
+
+
+
+
+    if __name__ == "__main__":
+        A = [2, 3, 7, 9, 1, 3, 4, ]
