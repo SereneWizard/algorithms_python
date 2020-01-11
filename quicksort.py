eaa5f7f76f45dab02c5cb5f58269c1cedfdebb04
SereneWizard <vaskardahal2015@gmail.com>
2020-01-10 23:33:23 -0800

    quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,36 +1,34 @@
-
-def partitioning(A, l, h):
+def partition(A, l, h):
     pivot = A[l]
-    i = l
-    j = h-1
-    while (i < j): 
-        while (A[i] < pivot):
+    i, j = l, h
+    while i < j:
+        while (A[i] <= pivot): 
             i += 1
+            if i > h: 
+                break
         while (A[j] > pivot):
             j -= 1
-        print(i,j)
-        if (i < j):
+            if j < l:
+                break
+        if i < j:
             A[i], A[j] = A[j], A[i]
-        
-    A[l], A[j] = A[j], A[l]
+    A[j], A[l] = A[l], A[j]
     return j
-        
 
+    
 def quicksort(B, l, h):
-    if (l < h):
-        j = partitioning(B, l, h)
-        print(B)
-        quicksort(B, l,j)
+    if l < h: 
+        j = partition (B, l, h)
+        quicksort(B, l, j)
         quicksort(B, j+1, h)
 
-def interface(B):
-    n = len(B)
-    l = 0
-    h = n
-    quicksort(B, l, h)
 
-if __name__ == "__main__":
-    A = [5, 10, 2, 12, 7, 15, 6, 3]
-    interface(A)
-    print("Hello")
-    print (A)
+if __name__ == "__main__":      
+    A = [10, 5, 2, 12, 1, 8, 18, 11, 7]
+    l = 0
+    r = len(A) 
+    print ("Start:")
+    print(A)
+    quicksort(A, l, r-1)
+    print("Final:")
+    print(A)
